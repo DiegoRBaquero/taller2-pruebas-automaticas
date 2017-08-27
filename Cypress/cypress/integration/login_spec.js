b5d1cc4e6efcac4b32f093cceed476edc0d3bc18
Diego Rodríguez <diegorbaquero@gmail.com>
2017-08-27 15:52:08 -0500

    Arreglo, alguien registro wrongemail

--- Cypress/cypress/integration/login_spec.js
+++ Cypress/cypress/integration/login_spec.js
@@ -4,8 +4,8 @@ describe('Los estudiantes login', function() {
       cy.contains('Cerrar').click()
       //Lineas nuevas
       cy.contains('Ingresar').click()
-      cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail@example.com")
-      cy.get('.cajaLogIn').find('input[name="password"]').click().type("1234")
+      cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail1@example.com")
+      cy.get('.cajaLogIn').find('input[name="password"]').click().type("12345")
       cy.get('.cajaLogIn').contains('Ingresar').click()
       cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
     })
